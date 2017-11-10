8ad3b3efa549ccdd8c5965d153191b0cf252d33a
zilikos <zilikoz@hotmail.com>
2017-11-10 13:29:38 -0600

    here

--- app/src/main/java/com/scolari/calendarproject/fragments/CalendarFragment.java
+++ app/src/main/java/com/scolari/calendarproject/fragments/CalendarFragment.java
@@ -1,18 +1,22 @@
 package com.scolari.calendarproject.fragments;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.firebase.client.Firebase;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,6 +25,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.scolari.calendarproject.Adapter.LeerActividadAdapter;
 import com.scolari.calendarproject.AnadirActividad;
 import com.scolari.calendarproject.Model.LeerActividad;
+import com.scolari.calendarproject.Model.ListaActividad;
 import com.scolari.calendarproject.R;
 
 import java.util.ArrayList;
@@ -33,15 +38,17 @@ public class CalendarFragment extends Fragment {
 
 
 
-    private static final String LISTA_NODE = " ListaActividades";
+    private static final String LIST_NODE = "ListaActividades";
     private static final String TAG = "CalendarFragment";
-    private DatabaseReference databaseReference;
     private ArrayAdapter arrayAdapter;
-    private Adapter adapter;
-    private List<LeerActividad> activitisNames;
+    private List<String> nombresActividades;
 
-    private RecyclerView rv;
+    private ListView lstActividad;
 
+    private static final String FIREBASE_SERVER_URL = "https://calendarproject-8d039.firebaseio.com/";
+
+    Firebase firebase;
+    private DatabaseReference databaseReference;
     //here
 
 
@@ -59,36 +66,37 @@ public class CalendarFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
-        rv = (RecyclerView)  view.findViewById(R.id.actividadRecycler);
-
-      //  RecyclerView actividadRecycler = (RecyclerView) view.findViewById(R.id.actividadRecycler);
-     //   activitisNames = new ArrayList<>();
-
-       // arrayAdapter = new ArrayA
-        // dapter(view.getContext(), R.layout.cardview_activity, activitisNames);
-
-        rv.setLayoutManager(new LinearLayoutManager(getContext()));
-        activitisNames = new ArrayList<>();
-   //     rv.setAdapter(LeerActividadAdapter);
 
 
+        lstActividad = (ListView)  view.findViewById(R.id.lstActividad);
+        nombresActividades = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter(view.getContext(), android.R.layout.simple_list_item_1, nombresActividades);
+        lstActividad.setAdapter(arrayAdapter);
 
-       // LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-       // linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-
-
-
-       // actividadRecycler.setLayoutManager(linearLayoutManager);
-        //arrayAdapter = new ArrayAdapter(getContext(), R.layout.cardview_activity, activitisNames);
-       // LeerActividadAdapter leerActividadAdapterr = new LeerActividadAdapter(buidPictures(), R.layout.cardview_activity, getActivity());
-    //    .setAdapter(arrayAdapter);
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();// Ejecuto la persistencia de firbase cuando esta sin internet
-    //    databaseReference = FirebaseDatabase.getInstance().getReference(); // esta liniea es para obtener el nodo principal padre
+        Firebase.setAndroidContext(view.getContext());
+        firebase = new Firebase(FIREBASE_SERVER_URL);
+        databaseReference = FirebaseDatabase.getInstance().getReference(LIST_NODE);
 
 
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                nombresActividades.clear();
+                if (dataSnapshot.exists()){
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        ListaActividad listaActividad = snapshot.getValue(ListaActividad.class);
+                        Log.w(TAG, "Actividades: " + listaActividad.getMateria());
+                        nombresActividades.add(listaActividad.getMateria());
+                    }
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
 
 
 
@@ -105,5 +113,6 @@ public class CalendarFragment extends Fragment {
 
         return view;
     }
+
 
 }
